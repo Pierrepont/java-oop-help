3a223faf9aa8dcefe71cee0d4d6bcb7262e3d491
FlorrieK <44760316+FlorrieK@users.noreply.github.com>
2018-12-05 22:25:01 -0500

    florriek_helloWorld.java

---
+++ florriek_helloWorld.java
@@ -0,0 +1,18 @@
+//This is an example
+
+public class helloWorld
+{
+    public static void main(String[] args)
+    {
+        Boolean thisProgramWorks = false;
+
+        if(thisProgramWorks)
+        {
+            System.out.println("Hello World!");
+        }
+        else
+        {
+            System.out.println("No.");
+        }
+    }
+}
