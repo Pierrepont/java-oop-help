a833e451cc81e529483b68b9dc5c7e1ce8e54101
Shastizzle <45697278+SRemiNM@users.noreply.github.com>
2018-12-10 06:49:45 -0800

    Update SRemiMN_insertionSortRecursive.java

--- SRemiMN_insertionSortRecursive.java
+++ SRemiMN_insertionSortRecursive.java
@@ -13,24 +13,27 @@ public class InserionSortRecursive {
     public static void main(String[] args) {
         int[]arr={2,91,52,4,3,1,34};
         int index=-1;
-        for(int i=1; i<arr.length; i++){
-            arr=check(arr,i);
-            //int temp=arr[index];
-            //arr[i]=temp;
+        for(int k=0; k<arr.length; k++){
+          for(int i=1; i<arr.length; i++){
+                arr=check(arr,i);
+          }
         }
         for(int i=0; i<arr.length; i++){
             System.out.print(arr[i]+" ");
         }
     }
-    public static int[] check(int[]arr, int i){
-        if(arr[i-1]<arr[i]){
+  //returns array with one element sorted  
+  public static int[] check(int[]arr, int i){
+      //checks to se if the element needs to be moved again  
+      if(arr[i-1]<arr[i]){
             return arr;
         }
-        else{
+      //moves element and runs method again if it can be moved
+       else{
             int temp = arr[i];
             arr[i] = arr[i-1];
-            temp=arr[i-1];
-            return check(arr,i-1);
+            arr[i-1]=temp;
+            return check(arr,i);
         }
     }
 }
